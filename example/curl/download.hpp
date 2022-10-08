cd4640ce3b720a1fdb9a8232a2f0b656a08bfcc1
Wyther-Yang <958700433@qq.com>
2022-10-08 17:11:24 +0800

    add the comments

--- example/curl/download.hpp
+++ example/curl/download.hpp
@@ -19,40 +19,49 @@ static constexpr auto DefaultMaxTimeOfWaiting = 2s;
 
         static IoCoro Active(Stream stream, char const* url)
         {
+                // guarantees the stream(socket) reclaimed by the ioCoro-context
                 unique_stream cleanup([&]{
                         if (!stream)
                                 printf("Download just finished...\n");
                 }, stream);
 
+                // initializes data chunk
                 DataChunk chunk{url};
 
+                // generates http request headers
                 string request;
 
                 request += "GET " + chunk.resrc_name + " HTTP/1.1\r\n";
                 request += "Host: " + chunk.Host + "\r\n\r\n";
                
+                // ensure REQUEST completion within the maximum time frame, and the time frame is 2s
                 {
                         DeadLine line([&]{
                                 stream.Close();
                                 printf("the CURL-request has been a timeout\n");
                         }, stream, DefaultMaxTimeOfWaiting);
                 
+                        // try to connect the server
                         co_await ioCoroConnect(stream, chunk.host.data());
                         if (CHECKINGERROR(stream))
                                 co_return;
 
+                        // try to send the Request Headers to the server, and then shutdown the Write Stream
                         co_await ioCoroCompletedWrite(stream, request.data(), request.size());
                         if (CHECKINGERROR(stream))
                                 co_return;
-                     
+
+                        // try to get the Reponse Headers
                         auto [num, pos] = co_await ioCoroReadUntil(stream, chunk.headers, 4096, "\r\n\r\n");
                         if (CHECKINGERROR(stream) || ::CHECKINGERROR(chunk))
                                co_return;
                         
+                        // try to get the reuqest entity body and shutdown the Read Stream
                         ssize_t ret = co_await ioCoroCompletedRead(stream, chunk.resrc.data(), chunk.resrc.size());
                         if (CHECKINGERROR(stream))
                                 co_return;
 
+                        // save the requested resources to the current directory
                         FILE* file = fopen(chunk.file_name.data(), "a");
                         fwrite(chunk.headers + pos + 4, 1, num - pos - 5, file);
                         fwrite(chunk.resrc.data(), 1, ret, file);
